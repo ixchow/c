bb3f0635e970e028b617d3742c985d693d4a7f0c
Jim McCann <ix@tchow.com>
2020-03-07 17:55:32 -0500

    preliminary flags.ko support

--- c.c
+++ c.c
@@ -6,7 +6,9 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <limits.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv, char **envp) {
 	char *ls = *argv;
@@ -17,10 +19,21 @@ int main(int argc, char **argv, char **envp) {
 		for ( ; *ls ; ++ls ) {
 			*ls = *(ls+1);
 		}
-		int rv = 0;
+		int rv = INT_MIN;
 		for (char **cp = envp; *cp; ++cp) {
 			if (0 == strncmp(*cp, "DOLLARSIGN_QUESTIONMARK_ALL_SPELLED_OUT=", 39)) {
 				rv = atoi(*cp + 40);
+			}
+		}
+		if (rv == INT_MIN) {
+			rv = 0;
+			//variable was not set, check kernel support:
+			int fd = open("/proc/flags/cf", O_RDONLY);
+			if (fd != -1) {
+				char c;
+				if (read(fd, &c, 1) == 1 && c == '1') {
+					rv = 1;
+				}
 			}
 		}
 		if (rv == 0) {
@@ -36,12 +49,16 @@ int main(int argc, char **argv, char **envp) {
 		puts(
 			"Usage:\n"
 			"    If called as `cNNN ...`:\n"
-			"        If DOLLARSIGN_QUESTIOMARK_ALL_SPELLED_OUT is nonzero:\n"
-			"            runs `NNN ...`\n"
-			"            returns DOLLARSIGN_QUESTIONMARK_ALL_SPELLED_OUT\n"
+			"        If DOLLARSIGN_QUESTIOMARK_ALL_SPELLED_OUT is set...\n"
+			"            ...and zero:\n"
+			"                runs `NNN ...` and returns 0\n"
+			"            ...otherwise:\n"
+			"                returns DOLLARSIGN_QUESTIONMARK_ALL_SPELLED_OUT\n"
+			"        Otherwise if /proc/flags/cf exists and does not contain '1':\n"
+			"            runs `NNN ...` and returns 0\n"
 			"        Otherwise:\n"
-			"            returns 0"
-			"    Otherwise: print this message and return 0.\n"
+			"            returns 0\n"
+			"    Otherwise: prints this message and return 0."
 		);
 	}
 	return 0;
